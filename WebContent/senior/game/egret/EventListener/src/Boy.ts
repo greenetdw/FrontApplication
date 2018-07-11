1294dd35761a3a65e826e86d80756bfe93281f7b
470277363@qq.com <470277363@qq.com>
2018-07-11 10:50:04 +0800

    egret learn

---
+++ WebContent/senior/game/egret/EventListener/src/Boy.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by wwtliu on 14/12/11.
+ */
+
+
+class Boy extends egret.Sprite{
+    public  constructor(){
+        super();
+    }
+    public order(){
+        //生成约会事件对象
+        var daterEvent:DateEvent = new DateEvent(DateEvent.DATE);
+        //添加相应的约会信息
+        daterEvent._year = 2014;
+        daterEvent._month = 12;
+        daterEvent._date = 11;
+        daterEvent._where= "极客学院";
+        daterEvent._todo = "共同学习";
+        //发送请求事件
+        this.dispatchEvent(daterEvent);
+    }
+}
---
+++ WebContent/senior/game/egret/L07_HttpUrl/src/NetPost.ts
@@ -0,0 +1,26 @@
+/**
+ * Created by wwtliu on 14/12/22.
+ */
+
+class NetPost extends egret.DisplayObjectContainer{
+    public constructor(){
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE,this.onAddToStage,this);
+    }
+    private onAddToStage(event:egret.Event){
+        var url:string = "http://httpbin.org/post";
+        var loader:egret.URLLoader = new egret.URLLoader();
+        loader.dataFormat = egret.URLLoaderDataFormat.VARIABLES;
+        loader.addEventListener(egret.Event.COMPLETE,this.onPostComplete,this);
+        var request:egret.URLRequest = new egret.URLRequest(url);
+        request.method = egret.URLRequestMethod.POST;
+        request.data = new egret.URLVariables("test=ok");
+        loader.load(request);
+    }
+
+    private onPostComplete(event:egret.Event):void{
+        var loader:egret.URLLoader = <egret.URLLoader> event.target;
+        var data:egret.URLVariables = loader.data;
+        console.log(data.toString);
+    }
+}
